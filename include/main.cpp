c6fd31d347ed4631bb4d5df02378b99daa0e0032
Lapalm <anthony_palm@hotmail.co.jp>
2018-02-19 18:49:55 +0000

    Added in clock function

--- include/main.cpp
+++ include/main.cpp
@@ -6,6 +6,7 @@
 #include "nodepath.h"
 #include <map>
 #include <iostream>
+#include <ctime>
 
 int main() {
 	//Load the Graph
@@ -22,21 +23,30 @@ int main() {
 	std::map<int, int> came_from_djikstra;
 	std::map<int, double> cost_so_far_djikstra;
 
-	
-	
-
 	int current = 63;
 	int goal = 0;
 	
 	std::cout << "==== A* ====" << std::endl << std::endl;
+
+	std::clock_t begin = std::clock();
 	astar.Find(came_from, cost_so_far);
+	std::clock_t end = std::clock();
+	double elapsed_secs = double(end - begin) / 1000;
+	std::cout << "Time taken to complete(A*): " << elapsed_secs << std::endl;
+
 	while (current != goal) {
 		std::cout << "From: " << current << " to " << came_from[current] << std::endl;
 		current = came_from[current];
 	}
 
 	std::cout << "==== Djikstra ====" << std::endl << std::endl;
+
+	begin = std::clock();
 	djikstra.Find(came_from_djikstra, cost_so_far_djikstra);
+	end = std::clock();
+	elapsed_secs = double(end - begin) / 1000;;
+	std::cout << "Time taken to complete(Djikstra): " << elapsed_secs << std::endl;
+
 	current = 63;
 	while (current != goal) {
 		std::cout << "From: " << current << " to " << came_from_djikstra[current] << std::endl;
